228dff4a70fd59f076bf50329d7d07326853fee1
Firejoust <42525540+Camezza@users.noreply.github.com>
2021-03-06 21:09:47 +1100

    Update points.js

--- points.js
+++ points.js
@@ -1,4 +1,4 @@
-let radius = 3;
+let radius = 6;
 let square_area = (1 + (radius * 2))**2;
 let previous_square_area = (1+((radius-1) * 2))**2;
 let checks = square_area - previous_square_area; // gets the points on the radius of the circle
@@ -21,7 +21,7 @@ let sine = Math.sin(2 * Math.PI * (i/il));
 let x = radius*(cosine)/(Math.max(Math.abs(cosine), Math.abs(sine)));
 let y = radius*(sine)/(Math.max(Math.abs(cosine), Math.abs(sine)));
 
-console.log(`At ${(i/il)*360}: (${Math.round(x)}, ${Math.round(y)})`);
+console.log(`At ${(i/il)*360}: (${Math.ceil(x)}, ${Math.ceil(y)})`);
 }
 console.timeEnd();
 console.log(point_history);
